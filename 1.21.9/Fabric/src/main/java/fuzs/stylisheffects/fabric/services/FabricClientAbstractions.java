84010823e48e0ade197d16c7b7d3da296628530b
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-10-06 23:00:49 +0200

    prepare 1.21.9 port

---
+++ 1.21.9/Fabric/src/main/java/fuzs/stylisheffects/fabric/services/FabricClientAbstractions.java
@@ -0,0 +1,54 @@
+package fuzs.stylisheffects.fabric.services;
+
+import fuzs.puzzleslib.fabric.api.event.v1.core.FabricEventInvokerRegistry;
+import fuzs.stylisheffects.api.v1.client.MobEffectWidgetContext;
+import fuzs.stylisheffects.api.v1.client.MobEffectWidgetEvents;
+import fuzs.stylisheffects.fabric.api.v1.client.FabricMobEffectWidgetEvents;
+import fuzs.stylisheffects.services.ClientAbstractions;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.item.TooltipFlag;
+
+import java.util.List;
+
+public final class FabricClientAbstractions implements ClientAbstractions {
+
+    @Override
+    public boolean renderInventoryText(MobEffectInstance effectInstance, AbstractContainerScreen<?> screen, GuiGraphics guiGraphics, int x, int y, int blitOffset) {
+        return false;
+    }
+
+    @Override
+    public boolean renderInventoryIcon(MobEffectInstance effectInstance, AbstractContainerScreen<?> screen, GuiGraphics guiGraphics, int x, int y, int blitOffset) {
+        return false;
+    }
+
+    @Override
+    public boolean renderGuiIcon(MobEffectInstance effectInstance, Gui gui, GuiGraphics guiGraphics, int x, int y, float z, float alpha) {
+        return false;
+    }
+
+    @Override
+    public boolean onEffectMouseClicked(MobEffectWidgetContext context, Screen screen, double mouseX, double mouseY, int button) {
+        return FabricMobEffectWidgetEvents.CLICKED.invoker()
+                .onEffectMouseClicked(context, screen, mouseX, mouseY, button)
+                .isInterrupt();
+    }
+
+    @Override
+    public void onGatherEffectTooltipLines(MobEffectWidgetContext context, List<Component> tooltipLines, TooltipFlag tooltipFlag) {
+        FabricMobEffectWidgetEvents.TOOLTIP.invoker().onGatherEffectTooltipLines(context, tooltipLines, tooltipFlag);
+    }
+
+    @Override
+    public void registerEventHandlers() {
+        FabricEventInvokerRegistry.INSTANCE.register(MobEffectWidgetEvents.MouseClicked.class,
+                FabricMobEffectWidgetEvents.CLICKED);
+        FabricEventInvokerRegistry.INSTANCE.register(MobEffectWidgetEvents.EffectTooltip.class,
+                FabricMobEffectWidgetEvents.TOOLTIP);
+    }
+}
